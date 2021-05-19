0f4e700c4ff6ca1ad0e1e9a33f038c85ee34204d
SuddenlyNothing <72772195+SuddenlyNothing@users.noreply.github.com>
2021-05-18 18:43:27 -0700

    Add MovingPlatform

---
+++ PlatformerGodot/Objects/MovingPlatform.gd
@@ -0,0 +1,18 @@
+extends Line2D
+
+onready var platform := $Platform
+onready var t := $Tween
+
+export(int) var speed := 10
+
+func _ready():
+	init_tween()
+
+func init_tween():
+	var end_point := get_point_position(1)
+	var duration = platform.position.distance_to(end_point)/speed
+	t.interpolate_property(platform, "position", platform.position, end_point, duration,
+		Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	t.interpolate_property(platform, "position", end_point, platform.position, duration,
+		Tween.TRANS_SINE, Tween.EASE_IN_OUT, duration)
+	t.start()
