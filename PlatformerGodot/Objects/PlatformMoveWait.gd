3c9975886a325d04074fdaa36f15bb3bf85b6a88
SuddenlyNothing <72772195+SuddenlyNothing@users.noreply.github.com>
2021-05-20 19:17:41 -0700

    Trying to fix PlatformMoveWait

--- PlatformerGodot/Objects/PlatformMoveWait.gd
+++ PlatformerGodot/Objects/PlatformMoveWait.gd
@@ -13,7 +13,7 @@ func _on_Area2D_body_entered(body):
 	t.repeat = true
 	if !t.is_active():
 		tween_two_way()
-	t.start()
+		t.start()
 
 func _on_Area2D_body_exited(body):
 	if body.name != "Player":
