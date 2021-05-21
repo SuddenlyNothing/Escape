314491b66ca00d5fec51c51b7a26b482be2752c1
SuddenlyNothing <72772195+SuddenlyNothing@users.noreply.github.com>
2021-05-20 23:25:58 -0700

    Fix Platform Player Detection

--- PlatformerGodot/Objects/PlatformMoveWait.gd
+++ PlatformerGodot/Objects/PlatformMoveWait.gd
@@ -1,22 +1,25 @@
 tool
 extends PlatformMove
 
-func _process(_delta):
+onready var player_detection := $PlayerDetection
+
+var player = null
+
+func _process(delta):
 	if Engine.editor_hint:
 		offset_end_point()
 		show_platform_end()
+	else:
+		if player == null and Global.player != null:
+			player = Global.player
+		if is_player_on_platform():
+			if !t.is_active():
+				tween_two_way()
+				t.start()
 
-func _on_Area2D_body_entered(body):
-	if body.name != "Player":
+func is_player_on_platform():
+	if player == null:
 		return
-	print("Enter")
-	t.repeat = true
-	if !t.is_active():
-		tween_two_way()
-		t.start()
-
-func _on_Area2D_body_exited(body):
-	if body.name != "Player":
-		return
-	print("Exit")
-	t.repeat = false
+	if player_detection.overlaps_body(player):
+		return true
+	return false
