200cbc13c64ea5805152664fdf2ae9e8cb1cccde
SuddenlyNothing <72772195+SuddenlyNothing@users.noreply.github.com>
2021-05-23 01:02:16 -0700

    Finish World and Level setup

--- PlatformerGodot/Objects/Checkpoint.gd
+++ PlatformerGodot/Objects/Checkpoint.gd
@@ -3,14 +3,20 @@ class_name Checkpoint
 
 onready var checkpoint_lit_sprite := $CheckpointLit
 onready var light := $Light2D
-onready var save_position = $SavePosition.position
+onready var save_position = $SavePosition.global_position
+onready var t := $Tween
+
+var lit := false
 
 func save_checkpoint():
 	pass
 
 func light_up():
-	checkpoint_lit_sprite.show()
-	light.enabled = true
+	if !lit:
+		checkpoint_lit_sprite.show()
+		t.interpolate_property(light, "energy", 0, 0.5, 0.5)
+		t.start()
+		lit = true
 
 func _on_Area2D_body_entered(body):
 	if body.name != "Player":
